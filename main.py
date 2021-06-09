5267d5c14493f2f573834a22ba9ae70e03ad2284
Uzii-hugo <noppanat.num@dome.tu.ac.th>
2021-06-09 14:09:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get('/bmi')
 # x:int:xxx (if none value x = xxx)
-def bmi(h:int=170,w:int=50):
+def bmi(h : int=170, w : int=50):
     h = (h/100) **2
     bmi = w/h
     return{'bmi': bmi}
