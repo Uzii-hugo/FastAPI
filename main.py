4ae599dcb48d5191cef2d5d05f7640041d5fd095
Uzii-hugo <noppanat.num@dome.tu.ac.th>
2021-06-09 14:08:11 +0700

    v1

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/bmi')
+# x:int:xxx (if none value x = xxx)
+def bmi(h:int=170,w:int=50):
+    h = (h/100) **2
+    bmi = w/h
+    return{'bmi': bmi}
