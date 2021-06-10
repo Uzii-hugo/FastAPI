4134e57e45ee759f907d2ee75ecebadd454d3b86
Uzii-hugo <noppanat.num@dome.tu.ac.th>
2021-06-10 23:48:40 +0700

    edit

--- main.py
+++ main.py
@@ -1,12 +1,55 @@
 from fastapi import FastAPI
-
+import json
+#-*- coding: utf-8 -*-
 app = FastAPI()
 
+  
 @app.get('/bmi')
 # x:int:xxx (if none value x = xxx)
-def bmi(h : int=170, w : int=50):
-    
-    h = (h/100) **2
+def bmi(h : int = 170, w: int = 50):
+
+    h   = (h/100) **2
     bmi = w/h
-    
+
     return{'bmi': bmi}
+
+@app.get('/riceall')
+def riceall():
+    data = json.load(open('data.json'))
+    return {'data':data}
+
+@app.get('/ricecalculator')
+# w = weight, u = unit of weight
+def cal_rice_price(type: str = 'tj', w: int = 10, u: str = 'kg'):
+    data = json.load(open('data.json'))
+    if u == 't':
+        result = data[type]['price']*w
+    else :
+        result = (data[type]['price']/1000)*w
+    return{
+        'type'      :type,
+        'name_eng'  :data[type]['nameeng'],
+        'weight'    :w,
+        'unit'      :u,
+        'price'     : result
+    }
+# ::hx = the humidity appears ::hy = the humidity need
+@app.get('/ricecalculator/humidity')
+def rice_humidity(type: str = 'tj', hx: int = 25, hy: int = 15):
+    data = json.load(open('data.json'))
+    w    = 1
+    if hy > 15:
+        w = 15*(hy - 15)
+    weight_humidity = (hx - hy)*w
+    balance         = 1000 - weight_humidity
+    result          = (data[type]['price'])/balance
+    return{
+        'type'      :type,
+        'name_eng'  :data[type]['nameeng'],
+        'weight'    :w,
+        'humidity'  :hx,
+        'dehumidify':hy,
+        'totol'     :result,
+        'unit'      :"bath/kg"
+    }
+    
